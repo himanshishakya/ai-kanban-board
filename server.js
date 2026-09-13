32b0bad345054e4b942b6a2bbf3bc9707e199008
himanshishakya <Himanshishakya688@gmail.com>
2026-09-13 12:55:13 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -206,7 +206,7 @@ io.on('connection', (socket) => {
 
 // 🔥 UPDATE 3: React Frontend ko serve karne ka code (Render ke liye)
 app.use(express.static(path.join(__dirname, 'dist')));
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
